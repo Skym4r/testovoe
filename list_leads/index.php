bf62059a9636d4063c25ceb1def64fd538b14c67
Dmitry <75675320+Skym4r@users.noreply.github.com>
2024-08-04 18:09:54 +0300

    Create index.php

---
+++ list_leads/index.php
@@ -0,0 +1,21 @@
+<? include '../php/header.php'?>
+<h3>Список лидов</h3>
+<div class="scrolling">
+    <table class='list' cellspacing="0" cellpadding="0">
+    
+        <tr>
+        <td class='heading'>ФИО</td>
+        <td class='heading'>Телефон</td>
+        <td class='heading'>Почта</td>
+        <td class='heading city' value="asc">Город &darr;</td>
+        </tr>
+        
+        <tr class='list_info'>
+
+        </tr>
+
+     </table>
+     </div>
+        <div class='btn'><button class='csv'>CSV</button></div>
+    
+<? include '../php/footer.php'?>
